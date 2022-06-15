213a39591dede1dd30c8fccb94e50d57a00a3734
Wyatt Howe <whowe@magnite.com>
2022-06-15 17:24:15 -0400

    Update __init__.py

--- lhe/__init__.py
+++ lhe/__init__.py
@@ -1,2 +1,12 @@
-"""Gives users direct access to the function."""
-from lhe.lhe import *
+from lhe.lhe import \
+	lhe,            \
+	keygen1,        \
+	keygen2,        \
+	encrypt1,       \
+	encrypt2,       \
+	add,            \
+	multiply,       \
+	decryptG1,      \
+	decryptG2,      \
+	decryptGT,      \
+	dlog
