9b10a3785e39b07c8f0d8c4254ee58ac2fdffa8d
Wyatt Howe <whowe@magnite.com>
2022-06-18 16:44:01 -0400

    Clean up exports

--- lhe/__init__.py
+++ lhe/__init__.py
@@ -1,10 +1,6 @@
 """This library exports everything needed to perform level-4 homomorphic encryption."""
-# from lhe.elgamal import \
-#   keygen,         \
-#   encrypt,        \
-#   decrypt
-# import lhe.elgamal as advanced
-# import lhe.level_d as leveled
+from lhe import elgamal, level_d
+
 
 def get_exports():
     import types
@@ -13,19 +9,18 @@ def get_exports():
     two_level = types.ModuleType('two-level fully homomorphic encryption')
     many_level = types.ModuleType('leveled fully homomorphic encryption')
 
-    import lhe.elgamal as elgamal
     two_level.keygen = elgamal.keygen
     two_level.encrypt = elgamal.encrypt
     two_level.decrypt = elgamal.decrypt
     two_level.advanced = elgamal
 
-    import lhe.level_d as level_d
     many_level.keygen = level_d.keygen
     many_level.encrypt = level_d.encrypt
     many_level.decrypt = level_d.decrypt
     many_level.advanced = level_d
 
     return two_level, many_level
+
 
 two_level, many_level = get_exports()
 del elgamal, level_d, get_exports
